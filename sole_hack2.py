fa82893d1815bdf8db90c5af632502d6bba4af03
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 16:59:27 +0100

    ggg

--- sole_hack2.py
+++ sole_hack2.py
@@ -62,8 +62,8 @@ if submitted:
     model = LinearRegression()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model.fit(X_train, y_train)
-    # Faire une prédiction avec le modèle
-    #prediction = model.predict(user_df)
+    #Faire une prédiction avec le modèle
+    prediction = model.predict(user_df)
 
-    # st.write('Votre score prédit est :', prediction[0])  
+    st.write('Votre score prédit est :', prediction[0])  
 
