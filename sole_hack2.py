ec72c907e311484ad09b91086451e02ff9531a41
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 19:57:58 +0100

    ddd

--- sole_hack2.py
+++ sole_hack2.py
@@ -12,14 +12,18 @@ df = pd.read_csv(url)
 st.title('Bienvenue dans le meilleur des mondes')
 st.header('Notez-vous les uns les autres')
 
-# Contenu principal centré
-col1, col2, col3 = st.columns([1, 3, 1])
+# Définition du code HTML/CSS pour définir une image en fond d'écran
+background = """
+<style>
+body {
+    background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Black_Mirror_logo.svg/1200px-Black_Mirror_logo.svg.png');
+    background-size: cover;
+}
+</style>
+"""
 
-# Affichage du titre, de l'en-tête et de l'image dans la partie centrale
-with col2:
-    image_path = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Black_Mirror_logo.svg/1200px-Black_Mirror_logo.svg.png"
-    st.image(image_path, caption="Your Image", use_column_width=800)
-    st.header('Prenez quelques minutes pour découvrir TOP5, notre nouveau système de notation du bonheur')
+# Affichage du code HTML/CSS avec st.markdown
+st.markdown(background, unsafe_allow_html=True)
 
 # Contenu dans la barre latérale à gauche
 with st.sidebar:
