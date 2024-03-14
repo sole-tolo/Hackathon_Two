005d1686237adb73802772be91ff3f11704760b9
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 16:44:24 +0100

    fff

--- sole_hack2.py
+++ sole_hack2.py
@@ -54,9 +54,6 @@ if submitted:
 
     # Entraîner le modèle
     model = LinearRegression()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model.fit(X_train, y_train)
-
     # Faire une prédiction avec le modèle
     prediction = model.predict(user_df)
 
