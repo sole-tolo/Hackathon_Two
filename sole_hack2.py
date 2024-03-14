e2340982da58ca2da100aba40c23edfcbf26a109
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 15:00:13 +0100

    fff

--- sole_hack2.py
+++ sole_hack2.py
@@ -13,3 +13,14 @@ df = pd.read_csv(url)
 # Mise en forme de la page
 st.title('Bienvenue dans le meilleur des mondes')
 st.header('Notez-vous les uns les autres')
+
+# interface utilisateur
+niveau_diplome = st.selectbox('Niveau de diplôme', ['Bac', 'Bac +2', 'Bac +3', 'Bac +5', 'Doctorat', 'Pas de diplôme'])
+genre = st.selectbox('Genre', ['Homme', 'Femme', 'Autre'])
+quartier = st.selectbox('Quartier', ['Centre-ville', 'Banlieue', 'Campagne'])
+classe_sociale = st.selectbox('Classe sociale', ['Classe moyenne', 'Classe supérieure', 'Classe populaire'])
+ecole_enfants = st.selectbox('École des enfants', ['Privée', 'Publique'])
+femme_menage = st.selectbox('Dispose de femme de ménage', ['Oui', 'Non'])
+casier_judiciaire = st.selectbox('Casier judiciaire', ['Vierge', 'Non vierge'])
+niveau_social_parents = st.selectbox('Niveau social des parents', ['Bac', 'Bac +2', 'Bac +3', 'Bac +5', 'Doctorat', 'Pas de diplôme'])
+cinema = st.radio('Va souvent au cinéma', ['Oui', 'Non'])
