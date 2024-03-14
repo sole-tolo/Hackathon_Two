05e99f95780874fb22d36408927412b3244f1715
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 20:03:54 +0100

    d

--- sole_hack2.py
+++ sole_hack2.py
@@ -10,11 +10,10 @@ df = pd.read_csv(url)
 
 # Mise en forme de la page
 st.title('Bienvenue dans le meilleur des mondes')
-st.header('Notez-vous les uns les autres')
+st.header('Prenez quelques minutes pour voter vos camarades')
 
 image_path = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Black_Mirror_logo.svg/1200px-Black_Mirror_logo.svg.png"
-st.image(image_path, caption="Your Image", use_column_width=1000)
-st.header('Prenez quelques minutes pour découvrir TOP5, notre nouveau système de notation du bonheur')
+
 
 # Contenu dans la barre latérale à gauche
 with st.sidebar:
