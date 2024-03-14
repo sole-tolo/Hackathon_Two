ee4c82a6c2116d5c9adf4a3b44fd99cadfb8e00c
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 18:01:36 +0100

    fff

--- sole_hack2.py
+++ sole_hack2.py
@@ -12,8 +12,8 @@ df = pd.read_csv(url)
 st.title('Bienvenue dans le meilleur des mondes')
 st.header('Notez-vous les uns les autres')
 
-# Contenu centré
-col1, _, col3 = st.columns([1, 3, 1])
+# Contenu principal centré
+col1, col2, col3 = st.columns([1, 3, 1])
 
 # Affichage du titre, de l'en-tête et de l'image dans la partie centrale
 with col2:
@@ -21,8 +21,10 @@ with col2:
     st.image(image_path, caption="Your Image", use_column_width=800)
     st.header('Prenez quelques minutes pour découvrir TOP5, notre nouveau système de notation du bonheur')
 
-# Création du formulaire
-with st.form("Quelle est ta valeur?"):
+# Contenu dans la barre latérale à gauche
+with st.sidebar:
+    st.title('Formulaire')
+    
     # Création des widgets
     options = {
         'Niveau de diplôme': ['Bac', 'Bac +2', 'Bac +3', 'Bac +5', 'Doctorat', 'Pas de diplôme'],
@@ -35,47 +37,48 @@ with st.form("Quelle est ta valeur?"):
         'Va souvent au cinéma': ['Oui', 'Non']
     }
 
-    # Création des widgets
-    user_data = {'Niveau de diplôme':'','Quartier':'','Salaire(€)':0,'Classe sociale':'','École des enfants':'','Dispose de femme de ménage':'', 
-                 "Prix au mètre carré de l'habitation(€/m2)":0,'Casier judiciaire':'',"Niveau social des parents":"",'Va souvent au cinéma':""}
-    user_data['Salaire(€)'] =st.number_input("Salaire(€)")
-    user_data["Prix au mètre carré de l'habitation(€/m2)"] =st.number_input("prix au m2 de ton lieu d'habitation")
+    with st.form("Quelle est ta valeur?"):
+        # Création des widgets
+        user_data = {'Niveau de diplôme':'','Quartier':'','Salaire(€)':0,'Classe sociale':'','École des enfants':'','Dispose de femme de ménage':'', 
+                     "Prix au mètre carré de l'habitation(€/m2)":0,'Casier judiciaire':'',"Niveau social des parents":"",'Va souvent au cinéma':""}
+        user_data['Salaire(€)'] =st.number_input("Salaire(€)")
+        user_data["Prix au mètre carré de l'habitation(€/m2)"] =st.number_input("prix au m2 de ton lieu d'habitation")
 
-    for key, values in options.items():
-        user_data[key] = st.selectbox(key, values)
-    submitted = st.form_submit_button("Yes, you are gorgeous")
+        for key, values in options.items():
+            user_data[key] = st.selectbox(key, values)
+        submitted = st.form_submit_button("Yes, you are gorgeous")
 
-# Si le formulaire est soumis, afficher les prédictions
-if submitted:
-    # Création du DataFrame avec les données utilisateur
-    user_data_dict = {}
-    for key, value in user_data.items():
-        user_data_dict[key] = value
-    user_df = pd.DataFrame([user_data_dict], index=[0])  # Spécifier l'index pour une seule ligne de données
+    # Si le formulaire est soumis, afficher les prédictions
+    if submitted:
+        # Création du DataFrame avec les données utilisateur
+        user_data_dict = {}
+        for key, value in user_data.items():
+            user_data_dict[key] = value
+        user_df = pd.DataFrame([user_data_dict], index=[0])  # Spécifier l'index pour une seule ligne de données
 
-    # j'encode les données utilisateur avec les mêmes catégories que celles de la df d'entraînement
-    label_encoder = LabelEncoder()
+        # j'encode les données utilisateur avec les mêmes catégories que celles de la df d'entraînement
+        label_encoder = LabelEncoder()
 
-    for colonne in user_df.select_dtypes(include=['object']).columns:
-        user_df[colonne] = label_encoder.fit(user_df[colonne]).transform(user_df[colonne])
+        for colonne in user_df.select_dtypes(include=['object']).columns:
+            user_df[colonne] = label_encoder.fit(user_df[colonne]).transform(user_df[colonne])
 
-    # je défini mes variables 
-    X = df.drop(columns=['Score','Age','Genre'])
-    y = df['Score']  
+        # je défini mes variables 
+        X = df.drop(columns=['Score','Age','Genre'])
+        y = df['Score']  
 
-    # Entraîner le modèle
-    model = LinearRegression()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model.fit(X_train, y_train)
+        # Entraîner le modèle
+        model = LinearRegression()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model.fit(X_train, y_train)
 
-    # Faire une prédiction avec le modèle
-    prediction = model.predict(user_df)
+        # Faire une prédiction avec le modèle
+        prediction = model.predict(user_df)
 
-    # Afficher les scores R² du modèle sur les ensembles d'entraînement et de test
-    st.write(model.score(X_train, y_train))
-    st.write(model.score(X_test, y_test))
-
-    # Afficher la prédiction au centre de la page
-    st.markdown('## Votre score prédit :')
-    st.image('https://previews.123rf.com/images/misteremil/misteremil1711/misteremil171100025/89192907-5-%C3%A9toiles-de-notation-isol%C3%A9es-sur-fond-blanc.jpg', width=100)
-    st.write(f'**{prediction[0]:.2f}**')
+        # Afficher la prédiction
+        st.markdown('## Votre score prédit :')
+        st.image('https://previews.123rf.com/images/misteremil/misteremil1711/misteremil171100025/89192907-5-%C3%A9toiles-de-notation-isol%C3%A9es-sur-fond-blanc.jpg', width=100)
+        st.write(f'**{prediction[0]:.2f}**')
+        # # Afficher la prédiction
+        # st.write('Votre score prédit est :', prediction[0])
+        # Afficher la prédiction au centre de la page
+   
