911e6d79d76d9952e3113293848d75983e9dcc3c
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 20:26:02 +0100

    fff

--- sole_hack2.py
+++ sole_hack2.py
@@ -8,10 +8,6 @@ from sklearn.model_selection import train_test_split
 url = 'https://raw.githubusercontent.com/sole-tolo/Hackathon_Two/main/data_hack2.csv'
 df = pd.read_csv(url)
 
-# Mise en forme de la page
-image_path = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Black_Mirror_logo.svg/1200px-Black_Mirror_logo.svg.png"
-st.image(image_path, caption="Your Image", use_column_width=1000)
-
 st.write("Votez-vous les uns les autres.")
 
 # Initialisation du score
