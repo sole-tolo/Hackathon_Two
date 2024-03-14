f500b0e91efdd4fa11bb18ec5b3908d1475c8b47
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 14:55:26 +0100

    ggg

--- sole_hack2.py
+++ sole_hack2.py
@@ -13,27 +13,3 @@ df = pd.read_csv(url)
 # Mise en forme de la page
 st.title('Bienvenue dans le meilleur des mondes')
 st.header('Notez-vous les uns les autres')
-
-# # Créer une interface utilisateur pour saisir les données personnelles
-# input_public = {}
-# for colonne in df.columns:
-#     if colonne != 'Score':
-#         valeur = st.text_input(colonne, "")
-#         input_public[colonne] = valeur
-
-# # Algo de recommandation
-# label_encoder = LabelEncoder()
-# for colonne in df.columns:
-#     if colonne != 'Score':
-#         input_public[colonne] = label_encoder.fit_transform([input_public[colonne]])[0]
-
-# X = df.drop(columns=['Score'])
-# y = df['Score']
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-
-# prediction = model.predict([list(input_public.values())])
-
-# st.write("Score prédit :", prediction[0])
-
