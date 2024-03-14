5425dcf82c162a372d8057f2eebe065f484bdc0f
sole-tolo <156327086+sole-tolo@users.noreply.github.com>
2024-03-14 14:35:20 +0100

    fff

--- sole_hack2.py
+++ sole_hack2.py
@@ -9,14 +9,3 @@ csv_content = response.text
 
 # Mise en forme de la page
 st.title('Bienvenue dans le meilleur des mondes')
-st.header("Notez-vous les uns les autres")
-
-t.title("Tu es seul.e pour cette Saint Valentin? Tu ne sais pas quoi manger, ni quoi écouter.. bref, tu es perdu.e?")
-st.header("Ici on t'aide avec quelques tips un peu bidon")
-# st.image(lien_image2)
-col1, col2,col3 = st.columns([1, 3, 1])
-col1.write("")
-image_path = "https://www.kideaz.com/wp-content/uploads/elementor/thumbs/moufles-couple-350-450-o8ist9xtyb036k7n6zgp5yv6igi8uvnrzmi24z5c74.jpg"
-col2.image(image_path, caption="Your Image", use_column_width=True)
-col2.write("")
-col3.write("")
